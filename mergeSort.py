d3a4a60075399e046ea33b220d0b6b0e21d3019a
alireza-zahiri <80680631+alirezazahiri@users.noreply.github.com>
2021-05-14 13:28:07 +0430

    mergeSort Done :white_check_mark:

---
+++ mergeSort.py
@@ -0,0 +1,32 @@
+def merge_sort(list: list, key='') -> list:
+    n = len(list)
+
+    if n <= 1:
+        return list
+    
+    mid = n // 2
+
+    L = merge_sort(list[:mid])
+    R = merge_sort(list[mid:])
+
+    return merge(L, R, key)
+
+
+def merge(list_one: list, list_two: list, key='') -> list:
+    list = []
+
+    while len(list_one) > 0 and len(list_two) > 0:
+        if list_one[0] < list_two[0]:
+            list.append(list_one[0])
+            list_one.pop(0)
+        else:
+            list.append(list_two[0])
+            list_two.pop(0)
+    
+    list += list_one
+    list += list_two
+
+    if key.casefold() == 'reverse':
+        list.reverse()
+
+    return list
